53ca5c22e2fc8f2990898053f29cd357fe82247f
Satvik Bajaj <satvik.bajaj31@gmail.com>
2019-01-24 14:54:16 -0500

    Add files via upload

---
+++ Initial_Commit.js
@@ -0,0 +1,2 @@
+console.log("This repository will showcase any freelance practice used to learn Javascript")
+console.log("Javascript was learned independently learned during my free time using the freeCodeCamp website")
